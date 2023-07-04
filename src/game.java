aaab754186420753ca2658ae4fe0a71fbeb278f2
Gus Raka <113882617+Oijin128@users.noreply.github.com>
2023-07-04 14:19:44 +0800

    Update Baru

--- src/game.java
+++ src/game.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class game {
+public class game {
+    private String namag;
+    private int harga;
+
+    public game(String namag, int harga) {
+        this.namag = namag;
+        this.harga = harga;
+    }
+
+    public game() {
+    }
+
+    public String getNama() {
+        return this.namag;
+    }
+
+    public int getHarga() {
+        return this.harga;
+    }
+
+    public void setNama(String namag) {
+        this.namag = namag;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
 }
